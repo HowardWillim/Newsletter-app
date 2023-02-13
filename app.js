7c7a816a71c4574dd07f0f5d7eed317b90cda5c9
HowardWillim <kurolimzz@gmail.com>
2023-02-13 20:56:46 +0700

    completing sign up form and the backend

--- app.js
+++ app.js
@@ -4,12 +4,17 @@ const bodyParser=require("body-parser");
 const app =express();
 app.use(bodyParser.urlencoded({extended:true}))
 
+app.use(express.static("public"));
 
 app.get("/",function(req,res){
-    
+    res.sendFile(__dirname+"/signup.html")
 })
 
-
+app.post("/",function(req,res){
+    var fname=req.body.fname;
+    var lname=req.body.lname;
+    var email=req.body.email;
+})
 app.listen(3000,function(){
     console.log("Server is running on port 3000");
 })
